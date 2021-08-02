81dcf07a15cfe2746aead79dbc4e43826a85357e
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2021-08-01 23:28:59 -0300

    Implemented libraries ScrollReveal and Swiper.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Making the menu icon on header interactive on click */
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -14,3 +16,47 @@ for (const link of links) {
     nav.classList.toggle('show')
   })
 }
+
+/* Changing header when the page's scrolling past the header's height */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/* Initializing Swiper for testimonials carousel */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+
+});
+
+/* ScrollReveal */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 600,
+  reset: true
+})
+
+scrollReveal.reveal(
+  ` 
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `, 
+  { interval: 100 }
+)
